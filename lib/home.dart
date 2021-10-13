4025128c5540dba57f4914c4e6bf0dc512347c5a
baiama <primkulovbaiaman@gmail.com>
2021-10-13 23:50:38 +0600

    dasd

--- lib/home.dart
+++ lib/home.dart
@@ -10,6 +10,13 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  double currentLeft = 0;
+  double savedLeft = 0;
+  late double startDeltaX;
+  late double xChange;
+  late final double maxX;
+  late final double oneStep;
+  late final double screenWidth;
   @override
   void initState() {
     super.initState();
@@ -17,6 +24,11 @@ class _HomeState extends State<Home> {
   }
 
   void _afterLayout(_) {
+    screenWidth = context.size!.width;
+    oneStep = (screenWidth - 40) / itemsCount;
+    maxX = (itemsCount) * oneStep;
+    savedLeft = oneStep * 0;
+    currentLeft = savedLeft + 10;
     setState(() {});
   }
 
@@ -47,13 +59,18 @@ class _HomeState extends State<Home> {
                   ),
                   Positioned(
                     bottom: 40,
-                    left: 0,
-                    child: Container(
-                      height: 20,
-                      width: 20,
-                      decoration: const BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: Colors.green,
+                    left: currentLeft,
+                    child: GestureDetector(
+                      onHorizontalDragStart: _onDragStart,
+                      onHorizontalDragUpdate: _onDrag,
+                      onHorizontalDragEnd: _onDragEnd,
+                      child: Container(
+                        height: 20,
+                        width: 20,
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.green,
+                        ),
                       ),
                     ),
                   ),
@@ -62,5 +79,27 @@ class _HomeState extends State<Home> {
             ),
           ],
         ));
+  }
+
+  void _onDrag(DragUpdateDetails details) {
+    var newOffset = details.globalPosition.dx;
+    xChange = newOffset - startDeltaX;
+
+    setState(() {
+      currentLeft = (xChange + savedLeft).clamp(0, maxX);
+      currentLeft = currentLeft + 10;
+    });
+  }
+
+  void _onDragEnd(DragEndDetails details) {
+    setState(() {
+      currentLeft = (currentLeft / oneStep).round() * oneStep;
+      currentLeft = currentLeft + 10;
+      savedLeft = currentLeft;
+    });
+  }
+
+  void _onDragStart(DragStartDetails details) {
+    startDeltaX = details.globalPosition.dx;
   }
 }
