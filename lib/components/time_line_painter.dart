3fc9cb7362a53784a96e9b779c88fad790f728fe
baiama <31430512+baiama@users.noreply.github.com>
2021-10-14 12:26:29 +0600

    Task 1 (#1)

---
+++ lib/components/time_line_painter.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+const bottomMargin = 60;
+const itemsCount = 8;
+
+class TimeLinePainter extends CustomPainter {
+  final int currentStep;
+  TimeLinePainter({required this.currentStep});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    //Draw top line
+    canvas.drawLine(
+      const Offset(0, 0),
+      Offset(size.width, 0),
+      pathPaint,
+    );
+    //Draw bottom line
+    canvas.drawLine(
+      Offset(0, size.height - bottomMargin),
+      Offset(size.width, size.height - bottomMargin),
+      pathPaint,
+    );
+
+    final oneItemWidth = size.width / itemsCount;
+
+    for (int i = 0; i <= itemsCount; i++) {
+      //Draw lines
+      var dx = (i * oneItemWidth);
+      var heightMargin = bottomMargin;
+      if (i == 0 || i == itemsCount) {
+        heightMargin = 20;
+      }
+      final heightEnd = size.height - heightMargin;
+      final p1 = Offset(dx, 0);
+      final p2 = Offset(dx, heightEnd);
+      Paint paint = pathPaint;
+      TextStyle style = textStyle;
+      if (currentStep == i) {
+        paint = pathPaintGreen;
+        style = textStyleGreen;
+      }
+      canvas.drawLine(p1, p2, paint);
+
+      var text = '${i * 3}:00';
+      if (i < 4) {
+        text = '0${i * 3}:00';
+      }
+
+      //Draw text
+      final textSpan = TextSpan(
+        text: text,
+        style: style,
+      );
+
+      final textPainter = TextPainter(
+        text: textSpan,
+        textDirection: TextDirection.ltr,
+      );
+
+      textPainter.layout(
+        minWidth: 0,
+        maxWidth: double.infinity,
+      );
+      final textDx = dx - textPainter.width / 2;
+      final offset = Offset(textDx, -20);
+      textPainter.paint(canvas, offset);
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
+  }
+
+  Paint get pathPaint {
+    var paint = Paint()..color = Colors.grey.withOpacity(0.5);
+    paint.strokeWidth = 1.5;
+    return paint;
+  }
+
+  Paint get pathPaintGreen {
+    var paint = Paint()..color = Colors.green;
+    paint.strokeWidth = 1.5;
+    return paint;
+  }
+
+  TextStyle get textStyle {
+    return const TextStyle(fontSize: 11, color: Colors.black);
+  }
+
+  TextStyle get textStyleGreen {
+    return const TextStyle(fontSize: 11, color: Colors.green);
+  }
+}
--- lib/home.dart
+++ lib/home.dart
@@ -1,10 +1,131 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+import 'components/time_line_painter.dart';
+
+class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  double currentLeft = 0;
+  double savedLeft = 0;
+  late double startDeltaX;
+  late double xChange;
+  late final double maxX;
+  late final double oneStep;
+  late final double screenWidth;
+  String text = '';
+  int currentStep = 0;
+  bool _isReady = false;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance!.addPostFrameCallback(_afterLayout);
+  }
+
+  void _afterLayout(_) {
+    text = '';
+    screenWidth = context.size!.width;
+    oneStep = (screenWidth - 40) / itemsCount;
+    maxX = (itemsCount) * oneStep;
+    savedLeft = oneStep * 3;
+    currentLeft = savedLeft + 10;
+    _isReady = true;
+    _updateText();
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    if (!_isReady) {
+      return Container();
+    }
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Time line'),
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const SizedBox(height: 200),
+            SizedBox(
+              height: 200,
+              width: double.infinity,
+              child: Stack(
+                children: [
+                  Positioned(
+                    left: 20,
+                    right: 20,
+                    child: SizedBox(
+                      height: 200,
+                      child: CustomPaint(
+                        painter: TimeLinePainter(currentStep: currentStep),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 40,
+                    left: currentLeft,
+                    child: GestureDetector(
+                      onHorizontalDragStart: _onDragStart,
+                      onHorizontalDragUpdate: _onDrag,
+                      onHorizontalDragEnd: _onDragEnd,
+                      child: Container(
+                        height: 20,
+                        width: 20,
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.green,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 30),
+            Text(
+              text,
+              style: const TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ));
+  }
+
+  void _onDrag(DragUpdateDetails details) {
+    var newOffset = details.globalPosition.dx;
+    xChange = newOffset - startDeltaX;
+
+    setState(() {
+      currentLeft = (xChange + savedLeft).clamp(0, maxX);
+      currentLeft = currentLeft + 10;
+      _updateText();
+    });
+  }
+
+  void _updateText() {
+    currentStep = (currentLeft / oneStep).round();
+    text = '${currentStep * 3}:00';
+    if (currentStep < 4) {
+      text = '0${currentStep * 3}:00';
+    }
+  }
+
+  void _onDragEnd(DragEndDetails details) {
+    _updateText();
+    setState(() {
+      currentLeft = (currentLeft / oneStep).round() * oneStep;
+      currentLeft = currentLeft + 10;
+      savedLeft = currentLeft;
+    });
+  }
+
+  void _onDragStart(DragStartDetails details) {
+    startDeltaX = details.globalPosition.dx;
   }
 }
